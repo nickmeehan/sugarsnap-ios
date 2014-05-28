d92e3b18d88a0feb8ae56bff547b7f3444253225
Nick Meehan <nnmeehan@gmail.com>
2014-05-28 15:28:43 -0700

    Took out commented out code in infinite scroller.

--- js/infiniteScroller.js
+++ js/infiniteScroller.js
@@ -1,21 +1,3 @@
-
-// var InfiniteScroller = (function() {
-//   var _informControllerOfScrollPosition = function(controller) {
-//     $(window).scroll(function() {
-//       if ($(window).scrollTop() >= ($(document).height() - 200) - $(window).height()) {
-//         controller.appendExtraPhotosOnScrollEvent()
-//       }
-//     });
-//   }
-//   return {
-//     checkScrollThreshold: function(controller) {
-//       _informControllerOfScrollPosition(controller)
-//     }
-//   }
-// })();
-
-
-
 var InfiniteScroller = (function() {
   var _informControllerOfScrollPosition = function(photoController) {
     window.onscroll = function() {
