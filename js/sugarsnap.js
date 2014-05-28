bca7954d32806bb9ca0c161ec3ea653aecf80d00
Nick Meehan <nnmeehan@gmail.com>
2014-05-28 10:55:37 -0700

    brought timeout down to 5 seconds.

--- js/sugarsnap.js
+++ js/sugarsnap.js
@@ -2,7 +2,7 @@ SugarSnap = {
 	initialize: function() {
 		alert("initialized")
 		new CameraController(new CameraView()).bindCameraListener()
-		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure.bind(this), {timeout: 10000, enableHighAccuracy: false, maximumAge: 75000})		
+		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure.bind(this), {timeout: 5000, enableHighAccuracy: false, maximumAge: 75000})		
 	},
 	getCoordinatesSuccess: function(position) {
 		// start spinner here
