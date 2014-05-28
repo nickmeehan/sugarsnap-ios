6a5cd2e43c9f286566f8f759b0d5cd86c5defd8d
Nick Meehan <nnmeehan@gmail.com>
2014-05-28 10:51:14 -0700

    changed timeout timing for locating user.

--- js/sugarsnap.js
+++ js/sugarsnap.js
@@ -2,7 +2,7 @@ SugarSnap = {
 	initialize: function() {
 		alert("initialized")
 		new CameraController(new CameraView()).bindCameraListener()
-		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure, {timeout: 30000, enableHighAccuracy: true, maximumAge: 75000})		
+		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure, {timeout: 10000, enableHighAccuracy: false, maximumAge: 75000})		
 	},
 	getCoordinatesSuccess: function(position) {
 		// start spinner here
